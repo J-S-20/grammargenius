0d2a661cfac9ccaada6ab1f740e81c247a6cc4e6
srujan1466 <srujan.24ec@kct.ac.in>
2025-04-29 23:51:55 +0530

    removed redundan files

--- php/register.php
+++ php/register.php
@@ -4,22 +4,17 @@ $username = "if0_38272767";
 $password = "ftpproject123"; 
 $dbname = "if0_38272767_grammargenius";
 
-// Create connection
 $conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Hash the password for security
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Insert into database
 $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $username, $hashed_password);
